1dd21853598bdd462600884f43c7bdf2d52ab1fa
Matias Garnica <garnicamatias@outlook.es>
2022-08-15 22:32:29 -0300

    prueba

--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from prueba import views as pruebas_views
+from portafolio import views as portafolio_views
+from contact import views as contact_views
 
 urlpatterns = [
+    path('', pruebas_views.home, name="home"),
+    path('about/', pruebas_views.about, name="about"),
+    path('portafolio/', portafolio_views.portafolio, name="portafolio"),
+    path('contact/', contact_views.contact, name="contact"),
     path('admin/', admin.site.urls),
+    path('accounts/',include('django.contrib.auth.urls')),
+
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
